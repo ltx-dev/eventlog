d3908afa5bf5b563cd171fcf948a4bc57774dcd7
ltx-dev <chrislotix@gmail.com>
2020-01-30 19:47:11 +0100

    Clean up main script, make a successful call from new ApiCall class

--- apicall.py
+++ apicall.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class ApiCall:
     def __init__(self, api_source):
@@ -6,8 +7,14 @@ class ApiCall:
 
     GITHUB_API = 'https://api.github.com'
     GITHUB_USER_EVENTS = '/users/ltx-dev/events' # events from my own repo for the time being
-    # STACKEXCHANGE API ADDRESS GOES HERE
+    STACKEXCHANGE_API = 'https://api.stackexchange.com/'
     STACKEXCHANGE_NETWORK_ACTIVITY = '/users/3485221/network-activity' # my own user id
 
-    def call(self.api_source):
+    def call(api_source):
+        if api_source == 'so':
+            response = requests.get('https://api.stackexchange.com/users/3485221/answers?site=stackoverflow')
+            parsed_response = json.loads(response.text)
+        
+        return parsed_response            
+
             
--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 # class ApiCall -> return json -> parse json -> create an event list item
-from test import Event
-import requests
-import json
+# from test import Event
+# import requests
+# import json
 
-API_URL = 'https://api.github.com'
+# API_URL = 'https://api.github.com'
 
-response = requests.get( API_URL + '/users/ltx-dev/events')
-parsed_response = json.loads(response.text) # loads deserializes json
+# response = requests.get( API_URL + '/users/ltx-dev/events')
+# parsed_response = json.loads(response.text) # loads deserializes json
 
-actor = parsed_response[0]['actor']
-type1 = parsed_response[0]['type']
-created_at = parsed_response[0]['created_at']
+# actor = parsed_response[0]['actor']
+# type1 = parsed_response[0]['type']
+# created_at = parsed_response[0]['created_at']
 
-event1 = Event(actor, type1, created_at)
-event_list = []    
+# event1 = Event(actor, type1, created_at)
+# event_list = []    
 
-for element in parsed_response:
-    event_list.append(Event(element['actor'], element['type'], element['created_at']))
+# for element in parsed_response:
+#     event_list.append(Event(element['actor'], element['type'], element['created_at']))
 
-print(event_list)
+# print(event_list)
 
+from apicall import *
+import pprint
+
+so = ApiCall.call('so')
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(so)
