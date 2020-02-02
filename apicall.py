fffd23700c8cd04aa7e3425393dc88c693f55f0e
ltx-dev <chrislotix@gmail.com>
2020-02-02 20:33:13 +0100

    Remove commented code, figure the bug with NameError for the constants inside ApiCall

--- apicall.py
+++ apicall.py
@@ -2,18 +2,21 @@ import requests
 import json
 
 class ApiCall:
-    def __init__(self, api_source):
-        self.api_source = api_source
-
     GITHUB_API = 'https://api.github.com'
     GITHUB_USER_EVENTS = '/users/ltx-dev/events' # events from my own repo for the time being
     STACKEXCHANGE_API = 'https://api.stackexchange.com/'
     STACKEXCHANGE_NETWORK_ACTIVITY = '/users/3485221/network-activity' # my own user id
 
+    def __init__(self, api_source):
+        self.api_source = api_source
+
     def call(api_source):
         if api_source == 'so':
             response = requests.get('https://api.stackexchange.com/users/3485221/answers?site=stackoverflow')
             parsed_response = json.loads(response.text)
+        elif api_source == 'github':
+            response = requests.get(GITHUB_API + GITHUB_USER_EVENTS)
+            parsed_response = json.loads(response.text)            
         
         return parsed_response            
 
